5857003db6437889506b730d6de9d6a00c15b2ef
Remant <remantmahato9798@gmail.com>
2022-04-13 01:01:09 +0530

    Added : UC3-EditContact

--- AddressBookCollection/AddressBookCollection/Program.cs
+++ AddressBookCollection/AddressBookCollection/Program.cs
@@ -67,7 +67,7 @@ namespace AddressBookCollection
                 AddressBook addresses = new AddressBook(FirstName.ToLower(), LastName, Address, City, State, ZipCode, PhoneNumber, Email);
                 contactList.Add(addresses);
                 contactsDetails.Add(FirstName.ToLower(), addresses);
-
+                
             }
             public void ComputeDetails()
             {
@@ -77,8 +77,36 @@ namespace AddressBookCollection
                 }
             }
 
-
-
+            public void EditContacts(string firstname)
+            {
+                if (contactsDetails.ContainsKey(firstname))
+                {
+                    Console.WriteLine("Enter your First Name: ");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("Enter your Last Name: ");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Enter your Address: ");
+                    string Address = Console.ReadLine();
+                    Console.WriteLine("Enter your city: ");
+                    string City = Console.ReadLine();
+                    Console.WriteLine("Enter your State: ");
+                    string State = Console.ReadLine();
+                    Console.WriteLine("Enter your Zip: ");
+                    string ZipCode = Console.ReadLine();
+                    Console.WriteLine("Enter your Phone Number: ");
+                    string PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter your Email: ");
+                    string Email = Console.ReadLine();
+                    AddressBook addresses = new AddressBook(FirstName.ToLower(), LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+                    contactList.Add(addresses);
+                    contactsDetails.Add(FirstName.ToLower(), addresses);
+                }
+                else
+                {
+                    Console.WriteLine("First Name doesnt exist");
+                }
+            }
+            
             static void Main(string[] args)
             {
                 Console.WriteLine("Welcome to Address Book Program");
@@ -88,28 +116,33 @@ namespace AddressBookCollection
                 {
                     Console.WriteLine("Choose 1: To Add a Contact");
                     Console.WriteLine("Choose 2: To compute Contacts");
+                    Console.WriteLine("Choose 3: To Edit a contact");
                     Console.WriteLine("Choose 0: To Exit");
-
-                    option = int.Parse(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            Details.AddContact();
-                            continue;
-                        case 2:
-                            Details.ComputeDetails();
-                            continue;
-
-                        case 0:
+                    
+                        option = int.Parse(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                Details.AddContact();
+                                continue;
+                            case 2:
+                                Details.ComputeDetails();
+                                continue;
+                            case 3:
+                                Console.WriteLine("Enter first name");
+                                string firstname = Console.ReadLine();
+                                Details.EditContacts(firstname);
+                                break;
+                             case 0:
                             Console.WriteLine("Exit");
                             break;
-
-                        default:
-                            Console.WriteLine("Choose valid Option");
-                            break;
-                    }
-
-
+                            
+                            default:
+                                Console.WriteLine("Choose valid Option");
+                                break;
+                        }
+                    
+                    
                 } while (option != 0);
             }
         }
